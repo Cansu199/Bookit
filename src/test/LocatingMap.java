b5e5426c138820f5b55126b0a8516f28b9e2cb5e
Cansu199 <58569648+Cansu199@users.noreply.github.com>
2020-02-29 15:55:31 -0500

    Update LocatingMap.java

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -3,5 +3,8 @@ package test;
 public class LocatingMap {
     public static void main(String[] args) {
         System.out.println("hey");
+        
+        //hello hello from github
+       
     }
 }
